63e7dea0a17e43ccf55b018bc51a4437b7ac6040
Alberto Guzman <albertog@garagecoders.net>
2024-07-14 13:29:38 -0600

    Patrones de diseño

---
+++ Patrones de diseño/index.js
@@ -0,0 +1,6 @@
+// Commonjs es la forma clasica de ver código en ejemplos pero ya no es recomendable 
+// ES Modules es la forma recomendable de usar en Node.js
+
+// .js --> por defecto utiliza CommonJS
+// .mjs --> para utilizar ES Modules
+// .js --> para utilizar CommonJs
