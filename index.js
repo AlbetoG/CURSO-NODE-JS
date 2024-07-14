1db783683e6374e3856f24729189898c154f5f07
Alberto Guzman <albertog@garagecoders.net>
2024-07-14 13:29:25 -0600

    Presentación

---
+++ index.js
@@ -0,0 +1,16 @@
+const descNodeJs = 'Node.js es un entorno de ejecución para JavaScript fuera del navegador.';
+console.log('¿Qué es Node.js?');
+console.log(descNodeJs);
+// En nodejs window no existe
+// console.log(typeof window);
+
+// En nodejs es global
+// console.log(global);
+
+// globalThis existe en nodejs y en el navegador
+// console.log(globalThis);
+
+// globalThis es l varuable global que siempre se usa para 'console' 'alert', 'math', 'fetch', ....
+// globalThis.console.log('Hola mundo');
+
+
